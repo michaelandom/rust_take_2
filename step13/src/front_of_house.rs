c6488eb89a5dab5fa5aa7e9f77b58d934c8478e6
michaelandom <michaelandom03@gmail.com>
2025-01-13 22:01:00 +0100

    step13

---
+++ step13/src/front_of_house.rs
@@ -0,0 +1,11 @@
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+
+     fn seat_at_waitlist() {}
+ }
+
+ pub mod serving {
+     fn take_order() {}
+     pub fn serve_order() {}
+     fn take_paymant() {}
+ }
--- step13/src/lib.rs
+++ step13/src/lib.rs
@@ -1,16 +1,11 @@
-mod front_of_house {
-    pub mod hosting {
-       pub fn add_to_waitlist() {}
+use rand::{Rng,CryptoRng,ErrorKind::Transient};
 
-        fn seat_at_waitlist() {}
-    }
 
-    pub mod serving {
-        fn take_order() {}
-        pub fn serve_order() {}
-        fn take_paymant() {}
-    }
-}
+// use std:io;
+use std::io::Write;
+use std::io::{self,Read};
+
+mod front_of_house;
  
 mod back_of_house{
     #[derive(Debug)]
