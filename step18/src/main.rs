28580136f61ffa2b84af25590ec107f5a990adcc
michaelandom <michaelandom03@gmail.com>
2025-01-17 15:35:00 +0100

    step18

--- step18/src/main.rs
+++ step18/src/main.rs
@@ -1,34 +1,45 @@
 fn main() {
-    let v = vec![1,2,3,5];
+    let v = vec![1, 2, 3, 5];
 
     let largest = get_largest(v);
 
-    println!("The largest num {}",largest);
+    println!("The largest num {}", largest);
 
-    let v = vec!['1','2','3','5'];
+    let v = vec!['1', '2', '3', '5'];
 
     let largest = get_largest(v);
 
-    println!("The largest char {}",largest);
+    println!("The largest char {}", largest);
 
-    let points = Points{x:2,y:5.3};
+    let points = Points { x: 2, y: 5.3 };
 
-    println!("The points char {:?}",points);
+    println!("The points char {:?}", points);
 
+    let points2 = Points { x: "Ss", y: "Sss" };
 
+    println!("The points2 char {:?}", points2.mix_up(points));
 }
 
 fn get_largest<T: PartialOrd + Copy>(v: Vec<T>) -> T {
-    let mut  l =v[0];
+    let mut l = v[0];
     for num in v {
         if num > l {
-            l =num;
+            l = num;
         }
     }
     l
 }
 #[derive(Debug)]
-struct Points<T,U> {
-    x:T,
-    y:U,
+struct Points<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Points<T, U> {
+    fn mix_up<V,W>(self, other: Points<V, W>) -> Points<T, W> {
+        Points {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
