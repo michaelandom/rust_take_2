a6b183ad080deb16ec6021b2f7425c2988580e4e
michaelandom <michaelandom03@gmail.com>
2025-01-17 15:18:47 +0100

    step18

---
+++ step18/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let v = vec![1,2,3,5];
+
+    let largest = get_largest(v);
+
+    println!("The largest num {}",largest);
+
+    let v = vec!['1','2','3','5'];
+
+    let largest = get_largest(v);
+
+    println!("The largest char {}",largest);
+}
+
+fn get_largest<T: PartialOrd + Copy>(v: Vec<T>) -> T {
+    let mut  l =v[0];
+    for num in v {
+        if num > l {
+            l =num;
+        }
+    }
+    l
+}
