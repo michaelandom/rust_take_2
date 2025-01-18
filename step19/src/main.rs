855ffdfa884df97874d5346b1aaf9bc4f4bcc4cd
michaelandom <michaelandom03@gmail.com>
2025-01-18 15:16:31 +0100

    step19

--- step19/src/main.rs
+++ step19/src/main.rs
@@ -1,74 +1,77 @@
-fn main() {
+use std::fmt::{Debug, Display};
 
-    let newArticle = NewArticle{ author: String::from("aaa"), headline: String::from("a"), content: String::from("asa")};
+fn main() {
+    let newArticle = NewArticle {
+        author: String::from("aaa"),
+        headline: String::from("a"),
+        content: String::from("asa"),
+    };
 
     let tweet = Tweet {
-        username:String::from("aaa"),
-        content:String::from("aaa"),
+        username: String::from("aaa"),
+        content: String::from("aaa"),
         reply: false,
         retweet: false,
     };
 
+    println!("newArticle {:?}", newArticle.summarize());
+    println!("Tweet {:?}", tweet.summarize());
 
-    println!("newArticle {:?}",newArticle.summarize());
-    println!("Tweet {:?}",tweet.summarize());
+    println!("newArticle test {:?}", newArticle.test());
+    println!("Tweet test {:?}", tweet.test());
 
-
-    println!("newArticle test {:?}",newArticle.test());
-    println!("Tweet test {:?}",tweet.test());
-    
-
-
-    println!("Tweet notify {:?}",notify(&tweet));
-
-    
+    println!("Tweet notify {:?}", notify(&tweet));
+    println!("Tweet notify_other {:?}", notify_other(&tweet,&newArticle));
 }
 
-
 pub struct NewArticle {
-    author:String,
-    headline:String,
-    content:String
+    author: String,
+    headline: String,
+    content: String,
 }
 
 impl Summary for NewArticle {
-
     fn summarize(&self) -> String {
-        format!("{}, by {}",self.content,self.author)
+        format!("{}, by {}", self.content, self.author)
     }
-    
 }
 pub fn notify<T: Summary>(item: &T) {
     println!("Breaking news! {}", item.summarize());
 }
 
-pub struct Tweet{
+pub fn notify2<T: Summary + Display>(item1: &T, item2: &T) {
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
+}
+
+pub fn notify_other<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    1
+}
+
+
+
+pub struct Tweet {
     username: String,
     content: String,
     reply: bool,
-    retweet: bool
+    retweet: bool,
 }
-
 
 impl Summary for Tweet {
-
     fn summarize(&self) -> String {
-        format!("{}, br {}", self.content,self.username)
+        format!("{}, br {}", self.content, self.username)
     }
-    
 }
-
 
 pub trait Summary {
-    fn summarize(&self) -> String{
+    fn summarize(&self) -> String {
         String::from("Read more ...")
     }
-    fn test(&self) -> String{
+    fn test(&self) -> String {
         String::from("Read more ...")
     }
 }
-
-
-
-
-
