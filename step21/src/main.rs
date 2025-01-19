a2416573abc9acea437b59d88c01ad9bfd95586f
michaelandom <michaelandom03@gmail.com>
2025-01-19 18:30:47 +0100

    step21

--- step21/src/main.rs
+++ step21/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     //    let r;
 
@@ -15,42 +17,50 @@ fn main() {
     println!("Hello, world! {}", result);
     let result2;
     {
-    let string3 = String::from("hesssssss");
+        let string3 = String::from("hesssssss");
 
-     result2 = longest(string1.as_str(), string3.as_str());
-
+        result2 = longest(string1.as_str(), string3.as_str());
     }
 
-    //`string3` does not live long enough 
+    //`string3` does not live long enough
 
     println!("Hello, world! {}", result2);
-    
 
-    let n =String::from("Call me you. as");
-    let s= n.split(".").next().expect("error");
+    let n = String::from("Call me you. as");
+    let s = n.split(".").next().expect("error");
 
-    let i =ImportantExcerpt {
-        part: s
-    };
-
+    let i = ImportantExcerpt { part: s };
 
     println!("Hello, world! {}", i.part);
 
 
+    //
 
+    let s: &'static str= "I have a static lifetime";
 }
 
-
-struct  ImportantExcerpt<'a> {
+struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
-
-// we cant send back local var ref 
+// we cant send back local var ref
 fn longest<'a>(string1: &'a str, string2: &'a str) -> &'a str {
     if string1.len() > string2.len() {
         string1
     } else {
         string2
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("longest_with_an_announcement {}", ann);
+
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
