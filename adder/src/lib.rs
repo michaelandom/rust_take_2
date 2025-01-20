8328431bdc140c9c5ca89df3e8582a5586f08f34
michaelandom <michaelandom03@gmail.com>
2025-01-20 20:02:04 +0100

    step22

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,15 +1,51 @@
 
+
+struct Rectangle {
+    wight: u32,
+    hight: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self,other: Rectangle) -> bool {
+        self.wight > other.wight && self.hight > other.hight
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2+2, 4);
+    fn larger_can_hold() {
+        let l = Rectangle {
+            wight:2,
+            hight:4
+        };
+
+        let s = Rectangle {
+            wight:1,
+            hight:2
+        };
+
+        assert!(l.can_hold(s));
     }
 
     #[test]
-    fn failing_test() {
-        panic!("Make this test fail");
+    fn small_can_hold() {
+        let l = Rectangle {
+            wight:2,
+            hight:4
+        };
+
+        let s = Rectangle {
+            wight:1,
+            hight:2
+        };
+
+        assert!(!s.can_hold(l));
     }
+
+    
 }
