54f43a222df44375d4e0bcf047d85cac5191818e
michaelandom <michaelandom03@gmail.com>
2025-01-21 17:26:01 +0100

    step23

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -21,7 +21,8 @@ struct Guess{
 }
 
 impl Guess {
-    // cargo test -- --test-threads=1
+    // cargo test -- --test-threads=1 
+    // cargo test -- --show-output
     fn new(value:i32) -> Guess {
         if value < 1 {
             panic!("value not allowed mut be > 1, but {} ", value )            
