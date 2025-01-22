06d6ebd4cfa3fa01078813bdfcd8533a1425c447
michaelandom <michaelandom03@gmail.com>
2025-01-22 21:02:14 +0100

    step24

--- minigrap/src/main.rs
+++ minigrap/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 fn main() {
     let arg: Vec<String>= env::args().collect();
     println!("{:?}",arg);
@@ -7,8 +8,11 @@ fn main() {
     let filename = &arg[2];
 
 
-    println!("{:?}",query);
-    println!("{:?}",filename);
+    println!("search word {}",query);
+    println!("file {}",filename);
 
+    let contant = fs::read_to_string(filename).expect("error no file");
+    
+    println!("with text:\n{}", contant);
 
 }
