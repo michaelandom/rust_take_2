b725a64cfe09a0fe633af2916df88f371b0f0146
michaelandom <michaelandom03@gmail.com>
2025-01-23 20:08:25 +0100

    step25

--- minigrap/src/main.rs
+++ minigrap/src/main.rs
@@ -4,14 +4,14 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("search word {}", config.query);
     println!("file {}", config.filename);
 
     if let Err(e) = minigrap::run(config) {
-        println!("problem reading file: {}", e);
+        eprintln!("problem reading file: {}", e);
         process::exit(1);
     }
 }
