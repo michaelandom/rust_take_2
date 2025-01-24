c03f14deb16ef4316fbc7281f4639ca708958d12
michaelandom <michaelandom03@gmail.com>
2025-01-24 18:59:29 +0100

    step26

--- step26/src/main.rs
+++ step26/src/main.rs
@@ -13,30 +13,54 @@ fn main() {
     generate_workout(simulated_intensity, simulated_random_number);
 }
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation:T) -> Cacher<T>{
+        Cacher {
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg:u32) -> u32 {
+
+        match self.value {
+            Some(v) => v,
+            None => {
+
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v 
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: i32) {
-    let value = |num|  {
+    let mut cacher_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         intensity
-    };
+    });
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups",
-            value(intensity)
-        );
+        println!("Today, do {} pushups", cacher_result.value(intensity));
 
-        println!(
-            "Next, do {} situps",
-            value(intensity)
-        );
+        println!("Next, do {} situps", cacher_result.value(intensity));
     } else {
-        if random_number ==3 {
+        if random_number == 3 {
             println!("Take a break Today")
         } else {
-            println!(
-                "Next, run for {} minutes!",
-                value(intensity)
-            );
+            println!("Next, run for {} minutes!", cacher_result.value(intensity));
         }
     }
 }
