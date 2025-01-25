2b6f7c2fa582b25b81554497d15f83e60deba5db
michaelandom <michaelandom03@gmail.com>
2025-01-25 19:02:22 +0100

    step27

--- step27/src/main.rs
+++ step27/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
 
 #[test]
-fn iterator_test(){
+fn iterator_test_into(){
     let v1 = vec![1,2,3];
 
     // let mut v1_iter = v1.iter_mut();
@@ -23,5 +23,33 @@ fn iterator_test(){
     assert_eq!(v1_iter.next(),Some( 1));
     assert_eq!(v1_iter.next(),Some( 2));
     assert_eq!(v1_iter.next(),Some( 3));
+    assert_eq!(v1_iter.next(),None);
+}
+
+#[test]
+fn iterator_test_mut(){
+    let mut v1 = vec![1,2,3];
+
+    // let mut v1_iter = v1.iter_mut();
+    // let mut v1_iter = v1.iter();
+    let mut v1_iter = v1.iter_mut();
+
+    assert_eq!(v1_iter.next(),Some(&mut 1));
+    assert_eq!(v1_iter.next(),Some(&mut 2));
+    assert_eq!(v1_iter.next(),Some(&mut 3));
+    assert_eq!(v1_iter.next(),None);
+}
+
+#[test]
+fn iterator_test(){
+    let mut v1 = vec![1,2,3];
+
+    // let mut v1_iter = v1.iter_mut();
+    // let mut v1_iter = v1.iter();
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(),Some(& 1));
+    assert_eq!(v1_iter.next(),Some(& 2));
+    assert_eq!(v1_iter.next(),Some(& 3));
     assert_eq!(v1_iter.next(),None);
 }
