400d3917bf54ac266f9b57fd086274649ff805c2
michaelandom <michaelandom03@gmail.com>
2025-01-25 19:03:53 +0100

    step27

--- step27/src/main.rs
+++ step27/src/main.rs
@@ -1,55 +1,62 @@
 fn main() {
-
-    let v1 = vec![1,2,3];
+    let v1 = vec![1, 2, 3];
 
     let v1_iter = v1.iter(); // lazey
-
 
     for value in v1_iter {
         println!("Got: {}", value)
     }
-
 }
 
-
 #[test]
-fn iterator_test_into(){
-    let v1 = vec![1,2,3];
+fn iterator_test_into() {
+    let v1 = vec![1, 2, 3];
 
     // let mut v1_iter = v1.iter_mut();
     // let mut v1_iter = v1.iter();
     let mut v1_iter = v1.into_iter();
 
-    assert_eq!(v1_iter.next(),Some( 1));
-    assert_eq!(v1_iter.next(),Some( 2));
-    assert_eq!(v1_iter.next(),Some( 3));
-    assert_eq!(v1_iter.next(),None);
+    assert_eq!(v1_iter.next(), Some(1));
+    assert_eq!(v1_iter.next(), Some(2));
+    assert_eq!(v1_iter.next(), Some(3));
+    assert_eq!(v1_iter.next(), None);
 }
 
 #[test]
-fn iterator_test_mut(){
-    let mut v1 = vec![1,2,3];
+fn iterator_test_mut() {
+    let mut v1 = vec![1, 2, 3];
 
     // let mut v1_iter = v1.iter_mut();
     // let mut v1_iter = v1.iter();
     let mut v1_iter = v1.iter_mut();
 
-    assert_eq!(v1_iter.next(),Some(&mut 1));
-    assert_eq!(v1_iter.next(),Some(&mut 2));
-    assert_eq!(v1_iter.next(),Some(&mut 3));
-    assert_eq!(v1_iter.next(),None);
+    assert_eq!(v1_iter.next(), Some(&mut 1));
+    assert_eq!(v1_iter.next(), Some(&mut 2));
+    assert_eq!(v1_iter.next(), Some(&mut 3));
+    assert_eq!(v1_iter.next(), None);
 }
 
 #[test]
-fn iterator_test(){
-    let mut v1 = vec![1,2,3];
+fn iterator_test() {
+    let mut v1 = vec![1, 2, 3];
 
     // let mut v1_iter = v1.iter_mut();
     // let mut v1_iter = v1.iter();
     let mut v1_iter = v1.iter();
 
-    assert_eq!(v1_iter.next(),Some(& 1));
-    assert_eq!(v1_iter.next(),Some(& 2));
-    assert_eq!(v1_iter.next(),Some(& 3));
-    assert_eq!(v1_iter.next(),None);
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    let sum: i32 = v1_iter.sum();
+
+    assert_eq!(sum, 6);
 }
