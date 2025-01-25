556d75a35e740e12a53676e8f2d6a96cac72c800
michaelandom <michaelandom03@gmail.com>
2025-01-25 19:38:07 +0100

    step27

--- step27/src/main.rs
+++ step27/src/main.rs
@@ -8,17 +8,18 @@ fn main() {
     }
 }
 
-
+#[derive(PartialEq,Debug)]
 struct Shoe {
     size: u32,
     style: String,
 }
 
 fn shoe_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    shoes.into_iter().filter(|shoe| shoe.size == shoe_size).collect()
+    shoes
+        .into_iter()
+        .filter(|shoe| shoe.size == shoe_size)
+        .collect()
 }
-
-
 
 #[test]
 fn iterator_test_into() {
@@ -68,16 +69,41 @@ fn iterator_sum() {
 
     let mut v1_iter = v1.iter();
 
-    let sum:i32 = v1_iter.sum();
+    let sum: i32 = v1_iter.sum();
 
     assert_eq!(sum, 6);
 }
 
-
 #[test]
 fn iterator_map() {
     let v1 = vec![1, 2, 3];
-    let v2: Vec<_> = v1.iter().map(|x | x + 1).collect();
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
-    assert_eq!(v2, [2,3,4]);
+    assert_eq!(v2, [2, 3, 4]);
+}
+
+#[test]
+fn shoe_in_my_size_test() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("a")
+        },
+        Shoe {
+            size: 15,
+            style: String::from("b")
+        },
+        Shoe {
+            size: 15,
+            style: String::from("b")
+        },
+    ];
+
+    let v2 = shoe_in_my_size(shoes, 10);
+    let ans = vec![Shoe {
+        size: 10,
+        style: String::from("a")
+    }];
+
+    assert_eq!(ans, v2)
 }
