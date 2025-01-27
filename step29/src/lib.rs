4d05ea087cc38f6f1685577f66a64608ebde531a
michaelandom <michaelandom03@gmail.com>
2025-01-27 12:25:15 +0100

    step29

---
+++ step29/src/lib.rs
@@ -0,0 +1,21 @@
+
+//! # My Create
+//! 
+//! 'step29' is a collocation of utilities to make performing certain
+//! calculation more convenient 
+
+/// Adds one to the number given.
+/// 
+/// # Examples
+/// 
+/// ```
+/// 
+/// let arg = 5;
+/// let answer = step29::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// 
+/// ```
+pub fn add_one(x:i32) -> i32 {
+    x + 1
+}
