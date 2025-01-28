222725b087811907ef0c6c3ab62ebb57ae1c769b
michaelandom <michaelandom03@gmail.com>
2025-01-28 21:02:23 +0100

    step30

--- step30/add-one/src/lib.rs
+++ step30/add-one/src/lib.rs
@@ -1,3 +1,4 @@
+use rand;
 pub fn add_one(num: i32) -> i32 {
     num + 1
 }
--- step30/adder/src/main.rs
+++ step30/adder/src/main.rs
@@ -1,4 +1,5 @@
 use add_one;
+use rand;
 fn main() {
     let num =2;
     println!("num {} and plus one {}",num,add_one::add_one(num));
