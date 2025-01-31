4584f91d338c508dce2c77ba950127c82396de57
michaelandom <michaelandom03@gmail.com>
2025-01-31 19:15:41 +0100

    step33

--- step33/src/main.rs
+++ step33/src/main.rs
@@ -1,5 +1,21 @@
 
+struct CustomSmartPointer{
+    data:String
+}
 
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`",self.data)
+    }
+}
 fn main() {
+    
+
+    let c = CustomSmartPointer{
+        data:String::from("some data")
+    };
+
+    println!("CustomSmartPointer created");
     
 }
