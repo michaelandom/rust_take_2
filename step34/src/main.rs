bac69d6a2f51f39db7c0b6609948ccd75cccf7b8
michaelandom <michaelandom03@gmail.com>
2025-02-01 22:15:57 +0100

    step34

--- step34/src/main.rs
+++ step34/src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
 
-    let a = Cons(10,Box::new(Cons(5,Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(3, Box::new(a));
+    let a = Rc::new(Cons(10,Rc::new(Cons(5,Rc::new(Nil)))));
+    let b = Cons(4, Rc::clone(&a));
+    let c = Cons(2, Rc::clone(&a));
 }
 
 
 enum List {
-    Cons(i32,Box<List>),
+    Cons(i32,Rc<List>),
     Nil
 }
+
+use std::rc::Rc;
 
 use crate::List::{Cons,Nil};
