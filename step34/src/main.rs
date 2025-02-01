cb3b5f0ea03e08e1383f90518d8cfe97cad0e02f
michaelandom <michaelandom03@gmail.com>
2025-02-01 22:13:15 +0100

    step34

--- step34/src/main.rs
+++ step34/src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
 
-    
+    let a = Cons(10,Box::new(Cons(5,Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(3, Box::new(a));
 }
+
+
+enum List {
+    Cons(i32,Box<List>),
+    Nil
+}
+
+use crate::List::{Cons,Nil};
