ca038223555cf4f1e9da31d68e2e886f9919359a
michaelandom <michaelandom03@gmail.com>
2025-02-02 22:54:02 +0100

    step35

--- step35/src/main.rs
+++ step35/src/main.rs
@@ -1,14 +1,31 @@
-use std::usize;
+use std::{cell::RefCell, rc::Rc, usize};
+use crate::List::{Cons,Nil};
 
-fn main() {
-    // let a = 5;
-    // let b = &mut a;
-
-    // let mut c =10;
-    // let d = &c;
-    // println!("d {}",*d);
-    // *d = 20; //cannot assign to `*d`, which is behind a `&` reference  `d` is a `&` reference, so the data it refers to cannot be written
+#[derive(Debug)]
+enum List {
+    Cons (Rc<RefCell<i32>>, Rc<List>),
+    Nil
 }
+fn main() {
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+
+    *value.borrow_mut() +=10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+
+
+   
+   }
 
 
 
