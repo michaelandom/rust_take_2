68adc59d025a80f108296d03ec7792077e24f033
michaelandom <michaelandom03@gmail.com>
2025-02-03 20:18:15 +0100

    step36

---
+++ step36_b/src/main.rs
@@ -0,0 +1,36 @@
+use std::{cell::RefCell, rc::{Rc, Weak}};
+
+fn main() {
+
+    let leaf = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![])
+    }); 
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 10,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]
+    )
+    });
+
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+
+}
+
+#[derive(Debug)]
+struct Node{
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>
+}
+
+
