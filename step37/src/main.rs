1221557ddc4cb821590c1a01a703b0c0bfacac91
michaelandom <michaelandom03@gmail.com>
2025-02-04 18:43:34 +0100

    step37

--- step37/src/main.rs
+++ step37/src/main.rs
@@ -1,17 +1,14 @@
 use std::{thread, time::Duration};
 
 fn main() {
-   let child = thread::spawn(|| {
 
-        for i in 1..10{
-            println!("Hi number {} from the spawned thread!",i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1,2,4];
+
+   let child = thread::spawn(move || {
+        println!("v in {:?}",v);
     });
-    
+
     child.join().unwrap();
-
-
     for i in 1..5{
         println!("Hi number {} from the main thread!",i);
         thread::sleep(Duration::from_millis(1));
