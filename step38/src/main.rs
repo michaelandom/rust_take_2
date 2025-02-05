287252ef11f0dbd4e06f4d5088437ec1d78cf63b
michaelandom <michaelandom03@gmail.com>
2025-02-05 20:51:53 +0100

    step38

--- step38/src/main.rs
+++ step38/src/main.rs
@@ -1,2 +1,22 @@
+
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
+
+
+    let (tx,rx) = mpsc::channel();
+
+
+     thread::spawn(move || {
+
+        let msg = String::from("hi");
+
+        tx.send(msg).unwrap();
+     });
+
+
+     let receiver = rx.recv().unwrap();
+
+     println!("Got: {}",receiver);
 }
