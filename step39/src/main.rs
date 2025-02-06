90a1e29a8b197d114606532db4def035096f3889
michaelandom <michaelandom03@gmail.com>
2025-02-06 20:15:18 +0100

    step39

--- step39/src/main.rs
+++ step39/src/main.rs
@@ -1,2 +1,18 @@
+use std::sync::Mutex;
+
 fn main() {
+
+    let x = Mutex::new(5);
+
+
+    {
+
+        let mut num = x.lock().unwrap();
+
+        *num = 6
+    }
+
+
+    println!("x in {:?}",x);
+
 }
