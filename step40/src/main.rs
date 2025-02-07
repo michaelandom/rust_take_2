7faba360f784f8ef16786fe669b6bfcb92ed7027
michaelandom <michaelandom03@gmail.com>
2025-02-07 21:13:31 +0100

    step40

--- step40/src/main.rs
+++ step40/src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
     
 }
+
+
+struct AveragedCollation {
+    list: Vec<i32>,
+    average: f32
+}
+
+
+impl AveragedCollation {
+
+    pub fn add(&mut self, value: i32){
+        self.list.push(value);
+        self.update_average();
+    }
+
+
+
+    pub fn remove(&mut self) -> Option<i32> {
+
+        let value = self.list.pop();
+
+        match value {
+            Some(num) => {
+                self.update_average();
+                Some(num)
+            },
+            None => None
+        }
+    }
+
+    pub fn average(&self) -> f32{
+        self.average
+    }
+
+    fn update_average(&mut self){
+
+        let total : i32= self.list.iter().sum();
+
+        let average = total as f32/ self.list.len() as f32;
+        self.average = average;
+        
+    }
+    
+}
