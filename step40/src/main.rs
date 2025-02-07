d430230772f76fb1d65f9fb1dc59b8ca5415d794
michaelandom <michaelandom03@gmail.com>
2025-02-07 21:18:36 +0100

    step40

--- step40/src/main.rs
+++ step40/src/main.rs
@@ -1,8 +1,26 @@
 fn main() {
-    
+    let mut ave = AveragedCollation::new();
+
+    ave.add(1);
+    ave.add(2);
+    ave.add(3);
+    ave.add(4);
+    ave.add(5);
+
+    println!("ava : {:?}",ave);
+
+    ave.remove();
+    ave.remove();
+    ave.remove();
+    ave.remove();
+    ave.remove();
+    println!("ava : {:?}",ave);
+
+
+
 }
 
-
+#[derive(Debug)]
 struct AveragedCollation {
     list: Vec<i32>,
     average: f32
@@ -10,6 +28,13 @@ struct AveragedCollation {
 
 
 impl AveragedCollation {
+
+    fn new() -> AveragedCollation {
+        AveragedCollation {
+            list: vec![],
+            average:0.0,
+        }
+    }
 
     pub fn add(&mut self, value: i32){
         self.list.push(value);
