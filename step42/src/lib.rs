50695f42034fb9bc5d5252b30dc3576a2ee1fbbb
michaelandom <michaelandom03@gmail.com>
2025-02-09 17:39:39 +0100

    step42

--- step42/src/lib.rs
+++ step42/src/lib.rs
@@ -1,5 +1,5 @@
 struct Post {
-    state: Box<dyn State>,
+    state: Option<Box<dyn State>>,
     contant: String
 }
 
@@ -7,18 +7,40 @@ impl Post {
 
     fn new() -> Post {
         Post {
-            state: Box::new(Draft {}),
+            state: Some(Box::new(Draft {})),
             contant: String::new()
         }
     }
     
+    pub fn content(&self) -> &str{
+        self.state.as_ref().unwrap().content(self)
+    }
 
     pub fn add_text(&mut self, value: &str) {
         self.contant.push_str(value);
     }
+
+    pub fn request_review(&mut self){
+
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.request_review());
+        }
+    }
+
+    pub fn approved(&mut self){
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.approve());
+        }
+    }
 }
 
 trait State {
+
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post:&'a Post) -> &'a str {
+        ""
+    }
     
 }
 
@@ -28,6 +50,46 @@ struct Draft{
 }
 
 impl State for Draft {
+
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    
+
 }
 
+struct PendingReview {
+}
 
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+    
+}
+
+struct Published{
+}
+
+impl State for Published {
+
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post:&'a Post) -> &'a str {
+         &post.contant
+    }
+}
