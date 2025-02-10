1a0cccf61b72ceb0740eb5ec96748ce5ae7959c1
michaelandom <michaelandom03@gmail.com>
2025-02-10 22:02:21 +0100

    step43

--- step43/src/main.rs
+++ step43/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
+
+    let language= Language::English;
+    match  language {
+
+        Language::English => println!("English"),
+        Language::Spanish => println!("Spanish"),
+        Language::Russian => println!("Russian"),
+        //Language::Japanese => println!("Japanese"),
+        lang => println!("Other {:?}", lang),
+        
+    }
     
+}
+#[derive(Debug)]
+enum Language {
+    English,
+    Spanish,
+    Russian,
+    Japanese
 }
