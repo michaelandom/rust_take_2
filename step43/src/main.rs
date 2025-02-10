900ac94b7d68088b64c659e4a605b83d3ebdb518
michaelandom <michaelandom03@gmail.com>
2025-02-10 22:08:01 +0100

    step43

--- step43/src/main.rs
+++ step43/src/main.rs
@@ -11,6 +11,25 @@ fn main() {
         // _ => println!("Other"),
         
     }
+
+    let authorization_status: Option<&str> = None;
+    let is_admin= false;
+    let group_id: Result<u8, _> = "34".parse();
+
+
+    if let Some(status) = authorization_status {
+        println!("Authorization status: {}", status);
+    } else if is_admin{
+        println!("Authorization status admin");
+    } else if let Ok(group_id) = group_id{
+
+        if group_id > 30{
+        println!("Authorization status privileged");
+        } else {
+            println!("Authorization status basic");
+        }
+    }
+
     
 }
 #[derive(Debug)]
