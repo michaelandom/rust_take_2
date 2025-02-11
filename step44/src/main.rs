3e85933e28677b2a2195b945a85377ac25be0cf5
michaelandom <michaelandom03@gmail.com>
2025-02-11 20:44:48 +0100

    step44

--- step44/src/main.rs
+++ step44/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let x = 5;
+    let x = Some(5);
+    let y = 5;
 
     match x {
-
-        1  => println!("1"),
-        2 => println!("3"),
-        3 => println!("2"),
-        4 => println!("4"),
+        Some(1)  => println!("1"),
+        Some(2) => println!("3"),
+        Some(y) => println!("y = {}",y),
         _ => println!("_")
     }
+
+    println!("y = {}",y);
 }
