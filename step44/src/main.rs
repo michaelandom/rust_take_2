a448ba0826bfeffb0d9c6b61f5671705628a7f05
michaelandom <michaelandom03@gmail.com>
2025-02-11 21:07:05 +0100

    step44

--- step44/src/main.rs
+++ step44/src/main.rs
@@ -36,10 +36,45 @@ fn main() {
     
     }
 
+
+    let message = Message::ChangeColor(1, 2, 3);
+
+    match message {
+        Message::Quit => {
+            println!("Quit");
+        },
+        Message::Move { x, y }=> {
+            println!("Move {}, {}",x,y);
+        },
+        Message::Write ( msg )=> {
+            println!("Write {}",msg);
+        },
+        Message::ChangeColor (Color::Rgb(r,g , b) )=> {
+            println!("ChangeColor Rgb {}, {}, {}",r,g,b);
+        },
+        Message::ChangeColor (Color::Hsv(r,g , b) )=> {
+            println!("ChangeColor Hsv  {}, {}, {}",r,g,b);
+        },
+
+
+        
+    }
+
 }
 
 struct Point{
     x:i32,
     y:i32,
+}
+
+enum Color{
+    Rgb(i32,i32,i32),
+    Hsv(i32,i32,i32)
+}
+enum Message{
+    Quit,
+    Move{x:i32,y:i32},
+    Write(String),
+    ChangeColor(Color)
 }
 
