035eee0b816e583f5bd2f72ff3689e15f00f2f21
michaelandom <michaelandom03@gmail.com>
2025-02-11 20:42:25 +0100

    step44

---
+++ step44/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 5;
+
+    match x {
+
+        1  => println!("1"),
+        2 => println!("3"),
+        3 => println!("2"),
+        4 => println!("4"),
+        _ => println!("_")
+    }
+}
