307893a218b103d85bb5c1ab8c7890c691e325f0
michaelandom <michaelandom03@gmail.com>
2025-02-13 20:58:19 +0100

    step45

--- step45/src/main.rs
+++ step45/src/main.rs
@@ -1,3 +1,4 @@
+use std::slice;
 fn main() {
    let mut num=5;
 
@@ -33,4 +34,21 @@ fn main() {
 
 
 
+}
+
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32] , &mut [i32]) {
+
+    let len = slice.len();
+    let ptr= slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len-mid),
+        )
+    }
+    
 }
