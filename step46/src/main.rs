c294e8d8338abc0b8d9c59ecef76ee2e42f2319c
michaelandom <michaelandom03@gmail.com>
2025-02-14 18:36:31 +0100

    step46

--- step46/src/main.rs
+++ step46/src/main.rs
@@ -7,6 +7,20 @@ fn main() {
 
 
 
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+    
+}
+
+
 struct Point{
     x:i32,
     y:i32,
