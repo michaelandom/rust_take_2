1966585293b8e01305d9da6e28600b4d18122a95
michaelandom <michaelandom03@gmail.com>
2025-02-14 18:25:36 +0100

    step46

--- step46/src/main.rs
+++ step46/src/main.rs
@@ -11,3 +11,13 @@ pub trait Iterator {
     fn next(&mut self) -> Option<Self::Item>;
     
 }
+
+struct Counter{}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(1)
+    }
+}
