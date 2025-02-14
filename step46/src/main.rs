dbfbc5c04547f2ead09917fef7076d3361015d62
michaelandom <michaelandom03@gmail.com>
2025-02-14 18:27:55 +0100

    step46

--- step46/src/main.rs
+++ step46/src/main.rs
@@ -4,20 +4,26 @@ fn main() {
 }
 
 
-pub trait Iterator {
+pub trait Iterator<T> {
 
-    type Item;
 
-    fn next(&mut self) -> Option<Self::Item>;
+    fn next(&mut self) -> Option<T>;
     
 }
 
 struct Counter{}
 
-impl Iterator for Counter {
-    type Item = u32;
+impl Iterator<u32> for Counter {
 
-    fn next(&mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<u32> {
+        Some(1)
+    }
+}
+
+
+impl Iterator<u16> for Counter {
+
+    fn next(&mut self) -> Option<u16> {
         Some(1)
     }
 }
