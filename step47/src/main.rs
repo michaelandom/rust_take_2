8b982a634f7f68a90e52e2a294e3ab952f0ce092
michaelandom <michaelandom03@gmail.com>
2025-02-15 21:50:01 +0100

    step47

--- step47/src/main.rs
+++ step47/src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt::{self, write};
+
 fn main() {
 
+    let wrapper=Wrapper(vec![String::from("a"),String::from("b")]);
+
+
+    println!("wrapper {}", wrapper);
 }
+
+
+struct Wrapper(Vec<String>);
+
+
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}",self.0.join(","))
+    }
+}
+
