6b6c2921336123cbb9564f091c498d1206c34aa3
michaelandom <michaelandom03@gmail.com>
2025-02-17 20:17:13 +0100

    step49

--- step49/src/main.rs
+++ step49/src/main.rs
@@ -1,6 +1,20 @@
 fn main() {
-
  let v = vec!["1","b"];
-
  let v1 = vec![1,2,3];  
+}
+
+
+#[macro_export]
+macro_rules! vec {
+    //  vec![1,2,3];  
+    ($($x:expr), *) => {
+
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
 }
